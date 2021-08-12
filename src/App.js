26c867d2376982e693975bd99e8d965968eee679
Jumperchick11 <Jumperchick1198@gmail.com>
2021-08-11 19:31:06 -0700

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Bethany Nixon and is{" "}
+        <a
+          href="https://github.com/Jumperchick11/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
