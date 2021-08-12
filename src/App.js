35554becebf4eb5d502edd5b16368bc9263fdbee
Jumperchick11 <Jumperchick1198@gmail.com>
2021-08-11 19:53:14 -0700

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Bethany Nixon and is{" "}
-        <a
-          href="https://github.com/Jumperchick11/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Bethany Nixon and is{" "}
+          <a
+            href="https://github.com/Jumperchick11/react-weather-app"
+            rel="noreferrer"
+            target="_blank"
+          >
+            open-sourced on github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
